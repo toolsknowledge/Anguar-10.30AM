1da6e8c79980d4c7853aad240626ec20364a4f84
Sambasivarao <samba.pendela@gmail.com>
2022-01-03 11:26:15 +0530

    directives

---
+++ directivesEx/src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+    //num:number = 0;
+    
+
+    /*
+    obj:any = {
+      "uname" : "",
+      "upwd" : ""
+    };
+
+    login():any{
+        if(this.obj.uname == "ashokit" && 
+            this.obj.upwd == "ashokit"){
+            alert("Login Success");
+        }else{
+          alert("Login Fail");
+        }
+    }
+    */
+
+
+   /* 
+   arr:any[] = [
+      {"p_id":111,"p_name":"p_one","p_cost":10000},
+      {"p_id":222,"p_name":"p_two","p_cost":20000},
+      {"p_id":333,"p_name":"p_three","p_cost":30000},
+      {"p_id":444,"p_name":"p_four","p_cost":40000},
+      {"p_id":555,"p_name":"p_five","p_cost":50000}
+   ];
+   */
+
+
+
+
+
+}
